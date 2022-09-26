891e51451cf6e4bc2bbd9f007e9118543a1693ab
vanessa <vanesska_denis2011@mail.ru>
2022-09-26 16:25:44 +0300

    Сделано ДЗ 1 в Уроке 5

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,16 @@ public class Main {
         int age = 22;
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием");
-        }
-        if (age < 18) {
+        }else{
             System.out.println("Возраст совершеннолетия не наступил, нужно еще подождать");
         }
         // Задание 2
         System.out.println("Задание 2");
         if (age >= 7 && age < 18) {
             System.out.println("Ребенок ходит в школу");
-        }
-        if (age >= 18 && age < 24) {
+        }else if (age >= 18 && age < 24) {
             System.out.println("Человек ходит в университет");
-        }
-        if (age >= 24) {
+        }else if (age>=24){
             System.out.println("Человек ходит на работу");
         }
         // Задание 3
@@ -29,15 +26,61 @@ public class Main {
         int stayUsed = 25;
         if (seatsUsed<seats){
             System.out.println("Есть еще "+(seats-seatsUsed)+" сидячих мест");
-        }
-        if (seats == seatsUsed) {
+        }else{
             System.out.println("Сидячих мест нет");
         }
         if (stayUsed<stay) {
             System.out.println("Есть еще " + (stay - stayUsed) + " стоячих мест");
-        }
-        if (stay==stayUsed){
+        }else{
             System.out.println("Стоячих мест нет");
+        }
+        // Hw3 Задание 1
+        System.out.println("Hw3 Задание 1");
+        int yearsOld = 16;
+        if (yearsOld>=2 && yearsOld<= 6){
+            System.out.println("Если возраст человека равен "+yearsOld+" , то ему нужно ходить в детский сад");
+        }else if(yearsOld>=6 && yearsOld<=18){
+            System.out.println("Если возраст человека равен "+yearsOld+" , то ему нужно ходить в школу");
+        }else if (yearsOld>=18 && yearsOld<=24){
+            System.out.println("Если возраст человека равен "+yearsOld+" , то ему нужно ходить в университет");
+        }else if (yearsOld>24){
+            System.out.println("Если возраст человека равен "+yearsOld+" , то ему нужно ходить на работу");
+        }
+
+        // Hw3 Задание 2
+        System.out.println("Hw3 Задание 2");
+        if (yearsOld<5){
+            System.out.println("Ребенок не может кататься на аттракционе");
+        }else if (yearsOld <14){
+            System.out.println("Ребенок может кататься только в сопровождении взрослого");
+        }else{
+            System.out.println("Ребенок может кататься без сопровождения взрослого");
+        }
+        // Hw3 Задание 3
+        System.out.println("Hw3 Задание 3");
+        int one = 8;
+        int two = -19;
+        int three = 98;
+        if (one>two){
+            if (one>=three) {
+                System.out.println("Максимальное число - " + one );
+            }else{
+                System.out.println("Максимальное число - " + three);
+            }
+        }else if (two>one){
+            if (two>=three) {
+                System.out.println("Максимальное число - " + two);
+            }else{
+                System.out.println("Максимальное число - " + three);
+            }
+        }else{
+            if (one>three){
+                System.out.println("Максимальное число - " + one);
+            }else if (three>one){
+                System.out.println("Максимальное число - " + three);
+            }else{
+                System.out.println("Все три числа равны");
+            }
         }
     }
 }
